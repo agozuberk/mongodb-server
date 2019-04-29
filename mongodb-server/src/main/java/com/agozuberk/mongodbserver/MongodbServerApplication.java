e0b61068c38463e80e0121a0cb55d56a47ea46d0
Ahmet Gozuberk <ahmet.gozuberk@pia-team.com>
2019-04-29 17:11:30 +0300

    first

---
+++ mongodb-server/src/main/java/com/agozuberk/mongodbserver/MongodbServerApplication.java
@@ -0,0 +1,13 @@
+package com.agozuberk.mongodbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbServerApplication.class, args);
+	}
+
+}
